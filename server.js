bad73e662625fd7d75d7ce7e370228f4c391c74e
bno229-max <bno229@gmail.com>
2026-07-21 19:01:23 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -77,6 +77,6 @@ app.get('/', (req, res) => {
 
 // Inicialização do Servidor
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
